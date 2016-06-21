23e255e9b18c3504e0fe7d7f713dd75ba2cf2c3e
Robert Hill <dr.pappa@gmail.com>
2016-06-21 21:27:40 +0100

    .

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// http://moduscreate.com/optimizing-react-es6-webpack-production-build/
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: './index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel']
+      }
+    ],
+  },
+};
